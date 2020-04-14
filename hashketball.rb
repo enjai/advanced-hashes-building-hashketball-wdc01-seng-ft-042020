35c43808a41e672e38cefc32a1d7cc4165e33bab
Injae Lee <ilee.injae@gmail.com>
2020-04-14 05:56:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,12 +2,24 @@
 
 def game_hash
   
-
   {:home =>
-    
-  ,  
+    {:team_name => "Brooklyn Nets", 
+     :colors => ["Black", "White"], 
+     :players => [
+       {:player_name => "Alan Anderson", :number => "", :shoe => "", :points => , :rebounds => , :assists => , :steals => , :blocks => :slam_dunks =>   }, 
+       {:player_name => " ", :number => "", :shoe => "", :points => , :rebounds => , :assists => , :steals => , :blocks => :slam_dunks =>   }, 
+       {:player_name => " ", :number => "", :shoe => "", :points => , :rebounds => , :assists => , :steals => , :blocks =>   :slam_dunks =>   }, 
+        {:player_name => " ", :number => "", :shoe => "", :points => , :rebounds => , :assists => , :steals => , :blocks => :slam_dunks =>   }, 
+        {:player_name => " ", :number => "", :shoe => "", :points => , :rebounds => , :assists => , :steals => , :blocks => :slam_dunks =>   }], 
   :away => 
-    
+      {:team_name => "Brooklyn Nets", 
+          :colors => ["Black", "White"], 
+          :players => [
+              {:player_name => " ", :number => "", :shoe => "", :points => , :rebounds => , :assists => , :steals => , :blocks => :slam_dunks =>   }, 
+             {:player_name => " ", :number => "", :shoe => "", :points => , :rebounds => , :assists => , :steals => , :blocks => :slam_dunks =>   }, 
+             {:player_name => " ", :number => "", :shoe => "", :points => , :rebounds => , :assists => , :steals => , :blocks =>   :slam_dunks =>   }, 
+             {:player_name => " ", :number => "", :shoe => "", :points => , :rebounds => , :assists => , :steals => , :blocks => :slam_dunks =>   }, 
+              {:player_name => " ", :number => "", :shoe => "", :points => , :rebounds => , :assists => , :steals => , :blocks => :slam_dunks =>   }], 
   } 
   
   
